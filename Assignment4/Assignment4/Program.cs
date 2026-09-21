e54b831043d69bdaa98ffba4903c2189387664fa
Mohamed Nabil <mohamed.nabil2592005@gmail.com>
2026-09-21 17:14:59 +0300

    feat:Part 1

---
+++ Assignment4/Assignment4/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Assignment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sessionNames =
+             {
+                "C# Basics",
+                "Arrays",
+                "Functions",
+                "Date and Time",
+                "Exception Handling"
+            };
+            DateTime[] sessionDates =
+            {
+                new DateTime(2026, 9, 12, 18, 0, 0),
+                new DateTime(2026, 9, 13, 18, 0, 0),
+                new DateTime(2026, 9, 17, 18, 0, 0),
+                new DateTime(2026, 9, 20, 18, 0, 0),
+                new DateTime(2026, 9, 24, 18, 0, 0)
+            };
+            int[] sessionDurations =
+            {
+                180,
+                240,
+                180,
+                240,
+                180
+            };
+           
+        }
+    }
+}
